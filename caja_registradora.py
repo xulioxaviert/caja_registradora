89ff444b560753563cc78a54084bf8285a65f5a7
Xulio Xaviert Rojas Teixeira <xulio.rojas@vasscompany.com>
2025-07-17 17:07:58 +0200

    first commit

---
+++ caja_registradora.py
@@ -0,0 +1,37 @@
+
+import datetime
+
+
+# caja_registradora.py
+
+ticket = [
+    "1 - beef steak - 30,23",
+    "4 - coca cola - 4,20",
+    "-2 - coca cola - 1,40",
+    "1 - bread - 0,90"
+]
+# función para calcular el importe total del ticket
+def calculate_total_amount(ticket):
+    total = 0.0
+    tax = 1.16
+    
+    #recorremos el ticket
+    for sale in ticket:
+        sale_details = sale.split(" - ")
+        units = int(sale_details[0])
+        product_name = sale_details[1].capitalize()
+        unit_price = float(sale_details[2].replace(",", "."))
+        
+        if units < 0:
+            total -= unit_price
+        else:
+            total += unit_price
+        # calculamos el total con impuestos
+        total_with_tax = total * tax
+        
+        print(f'Products: {product_name}, Units: {units}, Price: {unit_price * units}')
+    # mostramos solo el total con impuestos y fecha actual
+    print(f'Total: {total_with_tax}, Date: {datetime.datetime.now()}')
+
+
+calculate_total_amount(ticket)
